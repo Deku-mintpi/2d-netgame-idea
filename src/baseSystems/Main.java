b47151377477a60bc0804ae48e8a08f8578989fa
no name <negative97kh@gmail.com>
2016-05-12 10:51:55 -0700

    add some small stuff:

---
+++ src/baseSystems/Main.java
@@ -0,0 +1,12 @@
+package baseSystems;
+
+import com.badlogic.ashley.core.Engine;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Engine engine = new Engine();
+	}
+
+}
--- src/graphics/RenderSystem.java
+++ src/graphics/RenderSystem.java
@@ -1,13 +1,24 @@
 package graphics;
 
+import org.la4j.vector.dense.BasicVector;
+
 import com.badlogic.ashley.core.*;
 import com.badlogic.ashley.utils.*;
 import com.badlogic.gdx.graphics.glutils.*;
+
+import hypmath.HyperCoord;
 
 public class RenderSystem extends EntitySystem {
 	private ImmutableArray<Entity> Entities;
 	
 	public RenderSystem() {}
+	
+	public BasicVector toPoincare(HyperCoord pos) {
+		BasicVector pcoord = null;
+		pcoord.set(0, pos.x()/(1-pos.z()));
+		pcoord.set(1, pos.y()/(1-pos.z()));
+		return pcoord;
+	}
 	
 	
 	
